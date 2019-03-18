c861b2f4426bbe98a03d54ad20b4dc48013e77f5
q9liu <qiang.5.liu@nokia-sbell.com>
2019-03-18 16:51:47 +0800

    Add defination of object types

---
+++ ObjectType.hpp
@@ -0,0 +1,11 @@
+#ifndef __COMMON_OBJECT_TYPES_HPP__
+#define __COMMON_OBJECT_TYPES_HPP__
+
+Enum class EObjectType {
+    EObjectBlobType,
+    EObjectTreeType,
+    EObjectCommitType,
+    EObjectTagType
+};
+
+#endif
